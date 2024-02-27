a63e4a79f4dac9d275c538fbe5f946101ff936c7
Reya <150582472+Reya0707@users.noreply.github.com>
2024-02-27 22:57:03 +0000

    added some basic styling

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
+document.getElementById("myButton").addEventListener("click", myFunction);
 
-new Typewriter('#h1', {
-  strings: ['Hello', 'World'],
-  autoStart: true,
-});
 
 function showAnswer(response) {
-  alert(response.data.answer);
+  (response.data.answer);
 }
 
 let apiKey = "36c26f017e4ae34tb6a9679bb25odafe";
@@ -18,23 +15,6 @@ console.log(apiUrl);
 
 axios.get(apiUrl).then(showAnswer);
 
-let list = document.querySelector("danger");
-list.setAttribute("danger");
-
-let button = document.querySelector("#special-button");
-button.style.backgound = "red";
-
-console.log(button);
 
 
-let buttonOne = document.querySelector("#challenge-1");
-buttonOne.setAttribute("disabled", "disabled");
-
-
-  let buttonTwo = document.querySelector("#challenge-2");
-  buttonTwo.style.background = "green";
-
-
-let buttonThree = document.querySelector("#challenge-3");
-buttonThree.style.background = "red";
 
